9dc8e2b8e03e1d7a61a6111a97892c94f9261bc5
Dangremont Jayson <dangremontjayson.pro@gmail.com>
2022-01-29 12:04:17 +0100

    Add TOP_LEVEL_tb.vhd

---
+++ TOP_LEVEL_tb.vhd
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------------------
+--!
+--! \file TOP_LEVEL_tb.vhd
+--!
+--! \brief Testbenchs for TOP_LEVEL.
+--!
+--! \version 1.0
+--! \date XX/XX/XX
+--!
+--! \author Dangremont Jayson, dangremontjayson.pro@gmail.com
+--!
+----------------------------------------------------------------------------------
+
+--! Use simulation librairies
+Library IEEE;
+use IEEE.std_logic_TEXTIO.all;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
