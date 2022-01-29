375b562b4f8773d677d3519b54cf232d60d2edf2
Dangremont Jayson <dangremontjayson.pro@gmail.com>
2022-01-29 12:10:59 +0100

    Modify TOP_LEVEL_tb, add state machines

--- TOP_LEVEL_tb.vhd
+++ TOP_LEVEL_tb.vhd
@@ -16,3 +16,32 @@ Library IEEE;
 use IEEE.std_logic_TEXTIO.all;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+
+--! \brief Entity of TOP_LEVEL_tb
+entity TOP_LEVEL_HFECT_tb is
+    --  Port ( );
+end TOP_LEVEL_HFECT_tb;
+
+architecture simulation of TOP_LEVEL_HFECT_tb is
+
+    --! Component TOP_LEVEL declaration
+    component TOP_LEVEL_HFECT
+        port (
+           
+        );
+    end component;
+    
+    --! Signal declarations
+    
+begin
+    UUT: TOP_LEVEL_HFECT port map();
+
+    reset_gen: process
+        begin
+        end process;
+
+    clk_gen: process
+        begin
+        end process;
+
+end simulation;
